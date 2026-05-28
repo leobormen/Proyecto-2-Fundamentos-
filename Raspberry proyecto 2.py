ccc86ada06e4e7b6b0c88f7af8fe88401ae0fc11
leobormen <leo.borbon.mena@gmail.com>
2026-05-28 10:09:23 -0600

    Update Raspberry proyecto 2.py

--- Raspberry proyecto 2.py
+++ Raspberry proyecto 2.py
@@ -1,11 +1,16 @@
+import network
+import socket
 from machine import ADC, Pin, PWM
 import time
 
+SSID = "Leo" #No debe tener caracteres especiales
+PASSWORD = "l3575592l
 
 numeros_segmento = {"0" : [0,1,1,1,1,1,1], "1" : [0,0,0,1,0,0,1], "2" : [1,0,1,1,1,1,0], "3" : [1,0,1,1,0,1,1], "4" : [1,1,0,1,0,0,1], "5" : [1,1,1,0,0,1,1], "6" : [1,1,1,0,1,1,1], "7" : [0,0,1,1,0,0,1]}
 
 lista_productos = [0,2,5]
 
+servo = PWM(0)
 potenciometro = ADC(Pin(26))
 led1 = Pin(16, Pin.OUT)
 led2 = Pin(17, Pin.OUT)
@@ -20,11 +25,6 @@ segmento6 = Pin(20, Pin.OUT)
 
 diccionario_segmentos = {"segmento0" : segmento0, "segmento1" : segmento1, "segmento2" : segmento2, "segmento3" : segmento3, "segmento4" : segmento4, "segmento5" : segmento5, "segmento6" : segmento6}
 
-
-# Set up PWM Pin for servo control
-servo_pin = machine.Pin(0)
-servo = PWM(servo_pin)
-
 # Set Duty Cycle for Different Angles
 max_duty = 6800
 min_duty = 2000
@@ -33,6 +33,28 @@ half_duty = 4700
 #Set PWM frequency
 frequency = 50
 servo.freq (frequency)
+
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(SSID, PASSWORD)
+    
+    print("Conectando a WiFi...", end="")
+    while not wlan.isconnected():
+        print(".", end="")
+        time.sleep(0.5)
+    print("\nConectado:", wlan.ifconfig())
+    return wlan.ifconfig()[0]
+
+def start_server(ip):
+    s = socket.socket()
+    s.bind((ip, 1717))
+    s.listen(1)
+    print("Esperando conexión del cliente...")
+    conn, addr = s.accept()
+    print("Conectado desde:", addr)
+
 
 def mostrar_cantidad_productos(producto):
     numero = numeros_segmento[str(lista_productos[producto])]
